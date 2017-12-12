e2cb3cdfa6b897638f426d6b9b188138c782ca45
Mark24 <mark.zhangyoung@qq.com>
2017-12-12 09:55:31 +0800

    edit

--- tkEditor.py
+++ tkEditor.py
@@ -1,6 +1,8 @@
-from tkinter import Tk, Menu, Frame, Button, Scrollbar, Label, Text, messagebox, LEFT, RIGHT, X, Y, W, BOTH, BOTTOM, \
-    YES, NO, SUNKEN
+import os
+from tkinter import Tk, Menu, Frame, Button, Scrollbar, Label, Text, messagebox,filedialog, LEFT, RIGHT, X, Y, W, BOTH, BOTTOM, \
+    YES, NO, SUNKEN,END
 
+filename = ""
 
 def authorDef():
     messagebox.showinfo('作者信息', 'Mark24Code')
@@ -9,9 +11,60 @@ def authorDef():
 def aboutDef():
     messagebox.showinfo('版本信息', 'MIT LICENSE  Copyrighht. @2017 All rights reserved.')
 
+def openFileDef():
+    filename = filedialog.askopenfilename(defaultextension='*')
+    if filename == '':
+        filename = None
+    else:
+        root.title(string=filename)
+        textarea.delete(1.0,END)
+        f = open(filename,'r')
+        textarea.insert(1.0,f.read())
+        f.close()
+
+def newFileDef():
+    global filename
+    root.title(string="未命名文件")
+    filename = None
+    textarea.delete(1.0,END)
+
+def saveAsDef():
+    global filename
+    filename = filedialog.asksaveasfilename(initialfile='未命名.txt',defaultextension="*")
+    fh = open(filename,'w')
+    fcontent = textarea.get(1.0,END)
+    fh.write(fcontent)
+    fh.close()
+    root.title(string=filename)
+
+def saveDef():
+    global filename
+    try:
+        f = open(filename,'w')
+        fcontent = textarea.get(1.0,END)
+        f.write(fcontent)
+        f.close()
+    except:
+        saveAsDef()
+
+
+def cutDef():
+    textarea.event_generate('<<Cut>>')
+
+def copyDef():
+    textarea.event_generate('<<Copy>>')
+
+def pasteDef():
+    textarea.event_generate('<<Paste>>')
+
+def redoDef():
+    textarea.event_generate('<<Redo>>')
+
+def undoDef():
+    textarea.event_generate('<<Undo>>')
 
 root = Tk()
-root.title = 'tkEditor'
+root.title(string='tkEditor')
 root.geometry('500x500+100+100')
 
 # menu
@@ -19,19 +72,19 @@ menubar = Menu(root)
 root.config(menu=menubar)
 
 filemenu = Menu(menubar)
-filemenu.add_command(label="新建", accelerator="Ctrl+N")
-filemenu.add_command(label="打开", accelerator="Ctrl+O")
-filemenu.add_command(label="保存", accelerator="Ctrl+S")
-filemenu.add_command(label="另存为", accelerator="Ctrl+Shift+S")
+filemenu.add_command(label="新建", accelerator="Ctrl+N",command=newFileDef)
+filemenu.add_command(label="打开", accelerator="Ctrl+O",command=openFileDef)
+filemenu.add_command(label="保存", accelerator="Ctrl+S",command=saveDef)
+filemenu.add_command(label="另存为", accelerator="Ctrl+Shift+S",command=saveAsDef)
 menubar.add_cascade(label="文件", menu=filemenu)
 
 editmenu = Menu(menubar)
-editmenu.add_command(label="撤销", accelerator="Ctrl+Z")
-editmenu.add_command(label="重做", accelerator="Ctrl+Y")
+editmenu.add_command(label="撤销", accelerator="Ctrl+Z",command=undoDef)
+editmenu.add_command(label="重做", accelerator="Ctrl+Y",command=redoDef)
 editmenu.add_separator()
-editmenu.add_command(label="剪切", accelerator="Ctrl+X")
-editmenu.add_command(label="复制", accelerator="Ctrl+C")
-editmenu.add_command(label="粘贴", accelerator="Ctrl+V")
+editmenu.add_command(label="剪切", accelerator="Ctrl+X",command=cutDef)
+editmenu.add_command(label="复制", accelerator="Ctrl+C",command=copyDef)
+editmenu.add_command(label="粘贴", accelerator="Ctrl+V",command=pasteDef)
 editmenu.add_separator()
 editmenu.add_command(label="查找", accelerator="Ctrl+F")
 editmenu.add_command(label="替换", accelerator="Ctrl+R")
@@ -45,11 +98,12 @@ menubar.add_cascade(label="关于", menu=aboutmenu)
 
 # toolbar
 toolbar = Frame(root, height=25, bg='#24292E')
-openButton = Button(toolbar, text="打开")
-openButton.pack(side=LEFT, padx=5, pady=5)
 
-saveButton = Button(toolbar, text="保存")
+openButton = Button(toolbar, text="打开",command=openFileDef)
+openButton.pack(side=LEFT, padx=5, pady=5)
+saveButton = Button(toolbar, text="保存",command=saveDef)
 saveButton.pack(side=LEFT)
+
 toolbar.pack(expand=NO, fill=X)
 
 # statusbar
